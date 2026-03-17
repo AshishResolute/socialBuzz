8bf2d4b661c9da49f1fb893ce357c232dc95424b
AshishResolute <anshugourh21@gmail.com>
2026-03-17 20:39:19 +0530

    users can create their Account now

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,7 @@
 import express from "express";
 import joi from "joi";
-
+import db from "../../database/connection.js";
+import bcrypt from "bcrypt";
 const router = express.Router();
 
 // for signUp i need email,password,confirmPassword,userName => validate every inputs and store hashed Passwords
@@ -30,4 +31,27 @@ const signUpSchema = joi.object({
     .required(),
 });
 
+router.post("/signup", async (req, res) => {
+  try {
+    let { error, value } = signUpSchema.validate(req.body);
+    if (error) {
+      error.details.map((err) => console.log(err.message));
+      return res.status(400).json({ message: `Input Validation Failed` });
+    }
+    let { email, password, userName } = value;
+    let hashedPassword = await bcrypt.hash(password, 10);
+    let result = await db.query(
+      `insert into users(email,password,userName) values($1,$2,$3)`,
+      [email, hashedPassword, userName],
+    );
+    if (result.rowCount === 0)
+      return res.status(400).json({ message: `SignUp Failed!` });
+    res.status(200).json({ message: `SignUp Successfull!` });
+  } catch (err) {
+    console.log(`Error:${err.messsage}`);
+    res.status(500).json({ messageL: `Internal Server error` });
+  }
+});
 
+
+export default router;
--- src/routes/main.js
+++ src/routes/main.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import auth from './auth.js';
 const app = express();
 
 app.use(express.json());
@@ -8,5 +9,5 @@ app.get('/health',(req,res)=>{
     res.status(200).json({message:`Services Running Well,All Good!`});
 })
 
-
+app.use('/auth',auth)
 export default app
