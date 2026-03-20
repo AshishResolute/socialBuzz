4ea55962400930919c7724febe5a40709718682a
AshishResolute <anshugourh21@gmail.com>
2026-03-20 13:56:46 +0530

    feat:Now users can dislike a post as well

--- src/routes/likes.js
+++ src/routes/likes.js
@@ -1,4 +1,4 @@
-import express, { application } from "express";
+import express from "express";
 import { AppError } from "../../ErrorHandler/ErrorClass.js";
 import db from "../../database/connection.js";
 import verifyToken from "../middlewears/verifyToken.js";
@@ -19,19 +19,45 @@ router.post("/likePost/:PostId", verifyToken, async (req, res, next) => {
       `insert into likes(post_id,user_id) values($1,$2) returning id,liked_at`,
       [post_id, user_id],
     );
-    if (likeAPost.rowCount === 0)
-      return next(new AppError(`Post already Liked!`, 400));
-    res
-      .status(200)
-      .json({
-        message: `You successfully liked the post with Id${post_id}`,
-        likeId: likeAPost.rows[0].id,
-        Liked_at: likeAPost.rows[0].liked_at,
-      });
+    res.status(200).json({
+      message: `You successfully liked the post with Id${post_id}`,
+      likeId: likeAPost.rows[0].id,
+      Liked_at: likeAPost.rows[0].liked_at,
+    });
   } catch (err) {
+    if (err.code === "23505")
+      return next(new AppError(`Post already liked!`, 400));
     console.log(err);
     next(err);
   }
 });
 
-export default router
+router.delete("/unlikePost/:PostId", verifyToken, async (req, res, next) => {
+  try {
+    let user_id = req.user.id;
+    let post_id = parseInt(req.params.PostId);
+    if (isNaN(post_id)) return next(new AppError(`Invalid PostId!`, 400));
+    let findPost = await db.query(
+      `select id as like_id,post_id from likes where post_id=$1 and user_id=$2`,
+      [post_id, user_id],
+    );
+    if (findPost.rowCount === 0)
+      return next(new AppError(`Post not Found or Liked yet!`, 400));
+    let deletePost = await db.query(`delete from likes where id=$1`, [
+      findPost.rows[0].like_id,
+    ]);
+    if (deletePost.rowCount === 0)
+      return next(new AppError(`Post is still Liked,unlike Failed!`, 500));
+    res
+      .status(200)
+      .json({
+        message: `Post unliked successfully!`,
+        TimeStamp: `Unliked Post at ${new Date().toISOString()}`,
+      });
+  } catch (err) {
+    console.log(err.message);
+    next(err);
+  }
+});
+
+export default router;
