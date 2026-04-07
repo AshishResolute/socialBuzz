5c0cc36804fd6620ef0202a34f46b8afae84cd7c
AshishResolute <anshugourh21@gmail.com>
2026-04-07 11:12:06 +0530

    Added pagination on the feed route and now users can see the username for every posts made by the individual users

--- src/routes/feed.js
+++ src/routes/feed.js
@@ -8,14 +8,19 @@ const router = express.Router();
 
 router.get('/',verifyToken,async(req,res,next)=>{
     try{
+        console.log(req.query)
+        let page=parseInt(req.query.page)||1;
+        let limit = parseInt(req.query.limit)||3;
+        if(isNaN(page)||isNaN(limit)) return next(new AppError(`Invalid query Parameters provided`,400));
+        let offset = (page-1)*limit;
         let user_id=req.user.id;
-        let getAllPosts = await db.query(`select p.id,p.content,p.created_at,p.updated_at from posts as p join follow as f on p.user_id=f.following_id where f.follower_id=$1 order by p.updated_at desc`,[user_id]);
+        let getAllPosts = await db.query(`select p.id,p.content,u.username,p.created_at,p.updated_at from posts as p join follow as f on p.user_id=f.following_id join users as u on u.id=f.following_id where f.follower_id=$1 order by p.created_at desc limit $2 offset $3`,[user_id,limit,offset]);
         if(getAllPosts.rowCount===0) return res.status(200).json({
-            sucess:true,
+            success:true,
             message:`Nothing to display,Your Following list is empty!`
         })
         res.status(200).json({
-            sucess:true,
+            success:true,
             message:`The Posts are:`,
             posts:getAllPosts.rows,
             fetched_at:new Date().toLocaleString()
