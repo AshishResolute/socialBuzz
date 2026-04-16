bda7c388f9b445ef2b5eed640192a2b457b85299
AshishResolute <anshugourh21@gmail.com>
2026-04-16 20:52:14 +0530

    Completely tested the feed route

--- src/routes/feed.js
+++ src/routes/feed.js
@@ -1,36 +1,38 @@
-import express from 'express';
-import verifyToken from '../middlewears/verifyToken.js';
-import { AppError } from '../../ErrorHandler/ErrorClass.js';
-import db from '../../database/connection.js'
+import express from "express";
+import verifyToken from "../middlewears/verifyToken.js";
+import { AppError } from "../../ErrorHandler/ErrorClass.js";
+import db from "../../database/connection.js";
 const router = express.Router();
 
 // get all the posts from those who i follow
 
-router.get('/',verifyToken,async(req,res,next)=>{
-    try{
-        console.log(req.query)
-        let page=parseInt(req.query.page)||1;
-        let limit = parseInt(req.query.limit)||3;
-        if(isNaN(page)||isNaN(limit)) return next(new AppError(`Invalid query Parameters provided`,400));
-        let offset = (page-1)*limit;
-        let user_id=req.user.id;
-        let getAllPosts = await db.query(`select p.id,p.content,u.username,p.created_at,p.updated_at from posts as p join follow as f on p.user_id=f.following_id join users as u on u.id=f.following_id where f.follower_id=$1 order by p.created_at desc limit $2 offset $3`,[user_id,limit,offset]);
-        if(getAllPosts.rowCount===0) return res.status(200).json({
-            success:true,
-            message:`Nothing to display,Your Following list is empty!`
-        })
-        res.status(200).json({
-            success:true,
-            message:`The Posts are:`,
-            posts:getAllPosts.rows,
-            fetched_at:new Date().toLocaleString()
-        })
-    }
-    catch(err)
-    {
-        console.log(err.message);
-        next(err)
-    }
-})
+router.get("/", verifyToken, async (req, res, next) => {
+  try {
+    let page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 3;
+    if (isNaN(page) || isNaN(limit))
+      return next(new AppError(`Invalid query Parameters provided`, 400));
+    let offset = (page - 1) * limit;
+    let user_id = req.user.id;
+    let getAllPosts = await db.query(
+      `select p.id,p.content,u.username,p.created_at,p.updated_at from posts as p join follow as f on p.user_id=f.following_id join users as u on u.id=f.following_id where f.follower_id=$1 order by p.created_at desc limit $2 offset $3`,
+      [user_id, limit, offset],
+    );
+    if (getAllPosts.rowCount === 0)
+      return res.status(200).json({
+        success: true,
+        message: `Nothing to display,Your Following list is empty!`,
+      });
+    res.status(200).json({
+      success: true,
+      message: `The Posts are:`,
+      posts: getAllPosts.rows,
+      fetched_at: new Date().toLocaleString(),
+    });
+  } catch (err) {
+    console.log(err.message);
+    next(err);
+  }
+});
 
-export default router
+export default router;
---
+++ src/tests/feed.test.js
@@ -0,0 +1,83 @@
+import request from "supertest";
+import { describe, it, expect } from "@jest/globals";
+import pool from "../../database/connection.js";
+import redisConnection from "../../database/redis.js";
+import { emailQueue, postQueue } from "../queues/emailQueue.js";
+import app from "../routes/main.js";
+
+let token, followingUserId, followingUserToken;
+beforeAll(async () => {
+  await pool.query(`delete from follow`);
+  await pool.query(`delete from users`);
+  await pool.query(`delete from posts`);
+  await pool.query(`delete from comments`);
+  await pool.query(`delete from likes`);
+
+  await request(app).post(`/auth/signup`).send({
+    email: "feed@test.com",
+    password: `Feed@test.com`,
+    confirmPassword: `Feed@test.com`,
+    userName: `Feed Tester`,
+  });
+
+  await request(app).post(`/auth/signup`).send({
+    email: "follow@test.com",
+    password: `Follow@test.com`,
+    confirmPassword: `Follow@test.com`,
+    userName: `Following User`,
+  });
+
+  const getUserId = await pool.query(`select id from users where email=$1`, [
+    "follow@test.com",
+  ]);
+  followingUserId = getUserId.rows[0].id;
+
+  const getToken = await request(app)
+    .post("/auth/login")
+    .send({ email: `feed@test.com`, password: `Feed@test.com` });
+
+  const getFollowingUserToken = await request(app)
+    .post("/auth/login")
+    .send({ email: `follow@test.com`, password: `Follow@test.com` });
+
+  followingUserToken = getFollowingUserToken.body.token;
+
+  token = getToken.body.token;
+
+  const checkFollow = await request(app)
+    .post(`/follow/${followingUserId}`)
+    .set("Authorization", `Bearer ${token}`);
+
+  const checkPost = await request(app)
+    .post(`/post/content`)
+    .set("Authorization", `Bearer ${followingUserToken}`)
+    .send({
+      content: `This is my post which will be seen in the feed of my followers`,
+    });
+});
+
+afterAll(async () => {
+  await pool.query(`delete from follow`);
+  await pool.query(`delete from users`);
+  await pool.query(`delete from posts`);
+  await pool.query(`delete from comments`);
+  await pool.query(`delete from likes`);
+
+  await pool.end();
+  await redisConnection.quit();
+  await emailQueue.close();
+  await postQueue.close();
+});
+
+describe(`Feed Route`, () => {
+  describe(`GET /feed?page=1&limit=2`, () => {
+    it(`Should fetch all the posts from the following users with pagination`, async () => {
+      const res = await request(app)
+        .get(`/feed`)
+        .set("Authorization", `Bearer ${token}`);
+
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toHaveProperty("posts");
+    });
+  });
+});
