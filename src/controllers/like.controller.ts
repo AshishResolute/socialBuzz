ad4d3707be94a1db17713cf444e9ad0872dbec0d
AshishResolute <anshugourh21@gmail.com>
2026-07-17 20:14:53 +0530

    Migrate:like.controller:users can get totalLikes on a post

--- src/controllers/like.controller.ts
+++ src/controllers/like.controller.ts
@@ -1,49 +1,91 @@
-import type { Request,Response,NextFunction } from "express";
-import type {checkUserPostIdInterface} from '../interfaces/interfaces.js'
-import db from '../database/connection.js'
+import type { Request, Response, NextFunction } from "express";
+import type { checkUserPostIdInterface } from "../interfaces/interfaces.js";
+import db from "../database/connection.js";
 import { AppError, CheckIfDatabaseError } from "../ErrorHandler/ErrorClass.js";
 
-
-export const LikePost = async (req:Request<checkUserPostIdInterface>, res:Response, next:NextFunction):Promise<void> => {
+export const LikePost = async (
+  req: Request<checkUserPostIdInterface>,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
   try {
     let user_id = req.user?.id;
-    let post_id = req.params.postId
-   
-    const deleteLike = await db.query(`delete from likes where post_id=$1 and user_id=$2`,[post_id,user_id])
+    let post_id = req.params.postId;
 
-    if(deleteLike.rowCount){
+    const deleteLike = await db.query(
+      `delete from likes where post_id=$1 and user_id=$2`,
+      [post_id, user_id],
+    );
+
+    if (deleteLike.rowCount) {
       res.status(200).json({
-        success:true,
-        message:`Post like removed!`,
-        timeStamp:new Date().toISOString()
-      })
-      return
+        success: true,
+        message: `Post like removed!`,
+        timeStamp: new Date().toISOString(),
+      });
+      return;
     }
 
-    const  likeAPost = await db.query(
+    const likeAPost = await db.query(
       `insert into likes(post_id,user_id) values($1,$2) returning id,liked_at`,
-      [post_id, user_id]
+      [post_id, user_id],
     );
-    
-      // await emailQueue.add("emailQueue",{to:`${process.env.RESEND_USER_ACCOUNT_NAME}`})
+
+    // await emailQueue.add("emailQueue",{to:`${process.env.RESEND_USER_ACCOUNT_NAME}`})
 
     res.status(201).json({
-      success:true,
+      success: true,
       message: `You successfully liked the post with Id${post_id}`,
       likeId: likeAPost.rows[0].id,
       Liked_at: likeAPost.rows[0].liked_at,
     });
   } catch (error) {
-    if(CheckIfDatabaseError(error)){
-      console.error(`DataBase Error:${error.message}`)
-     next(new AppError(error.message,500))
-      return
-    }
-    else if(error instanceof Error){
-      console.error(`Standard App Error`)
-      next(new AppError(error.message,500))
-      return
+    if (CheckIfDatabaseError(error)) {
+      console.error(`DataBase Error:${error.message}`);
+      next(new AppError(error.message, 500));
+      return;
+    } else if (error instanceof Error) {
+      console.error(`Standard App Error`);
+      next(new AppError(error.message, 500));
+      return;
     }
     next(error);
   }
-}
+};
+
+export const getTotalLikesOnPost = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const post_id = req.params.postId;
+    const totalLikes = await db.query(
+      `select count(post_id) as totalLikes from likes where post_id=$1`,
+      [post_id],
+    );
+    console.log(totalLikes.rows)
+    if (!parseInt(totalLikes.rows[0].totalLikes)) {
+      res.status(404).json({
+        message: `Post Not found or not yet liked yet!`,
+      });
+      return;
+    }
+    res.status(200).json({
+      success: true,
+      message: `Total Likes:${totalLikes.rows[0].totallikes}`,
+      timeStamp: new Date().toISOString(),
+    });
+  } catch (error) {
+    if (CheckIfDatabaseError(error)) {
+      console.error(`Database Error:${error.message}`);
+      next(new AppError(error.message, 500));
+      return;
+    } else if (error instanceof Error) {
+      console.error(`Standard App Error:${error.message}`);
+      next(new AppError(error.message, 500));
+      return;
+    }
+    next(error);
+  }
+};
--- src/routes/likes.ts
+++ src/routes/likes.ts
@@ -2,7 +2,7 @@ import express from "express";
 import {checkUserPostId} from '../Validator/Validator.js'
 import { validate } from "../Middlewares/joiValidator.js";
 import verifyToken from "../Middlewares/verifyToken.js";
-import { LikePost } from "../controllers/like.controller.js";
+import { getTotalLikesOnPost, LikePost } from "../controllers/like.controller.js";
 const router = express.Router();
 
 router.post("/likePost/:postId", verifyToken,validate({params:checkUserPostId}),LikePost);
@@ -33,30 +33,6 @@ router.post("/likePost/:postId", verifyToken,validate({params:checkUserPostId}),
 //   }
 // });
 
-// router.get("/totalLikes/:postId", verifyToken, async (req, res, next) => {
-//   try {
-//     let user_id = req.user.id;
-//     let post_id = parseInt(req.params.postId);
-//     if (isNaN(post_id))
-//       return next(new AppError(`Invalid post_id recieved`, 400));
-
-//     let findPost = await db.query(`select id from posts where id=$1`, [
-//       post_id,
-//     ]);
-//     if (findPost.rowCount === 0)
-//       return next(new AppError(`Post not Found or not yet liked!`, 404));
-//     let totalLikes = await db.query(
-//       `select count(user_id) as likes_count from likes where post_id=$1`,
-//       [post_id],
-//     );
-//     res.status(200).json({
-//       message: `Your Post has ${totalLikes.rows[0].likes_count} Likes`,
-//       TimeStamp: new Date().toISOString(),
-//     });
-//   } catch (err) {
-//     console.log(err.message);
-//     next(err);
-//   }
-// });
+router.get('/getLikes/:postId',validate({params:checkUserPostId}),getTotalLikesOnPost)
 
 export default router;
