7e823becaba1f96964c59929148d84ecf81a210b
AshishResolute <anshugourh21@gmail.com>
2026-09-02 20:02:39 +0530

    Added the generic query function to handle quries and errors to remove mess in catch block in each controllers

--- src/database/query.ts
+++ src/database/query.ts
@@ -1,3 +1,8 @@
+import {
+  CheckIfDatabaseError,
+  ClientError,
+  DataBaseErrors,
+} from "../ErrorHandler/ErrorClass.js";
 import db from "./connection.js";
 import type { QueryResult, QueryResultRow } from "pg";
 
@@ -5,5 +10,19 @@ export const query = async <T extends QueryResultRow>(
   text: string,
   params?: any[],
 ): Promise<QueryResult<T>> => {
-    return db.query<T>(text,params)
+  try {
+    return await db.query<T>(text, params);
+  } catch (error) {
+    if (CheckIfDatabaseError(error)) {
+      if (error.code === "23505") {
+        throw new ClientError(
+          `Duplicate entry`,
+          400,
+          `Account already exists!`,
+        );
+      }
+      throw new DataBaseErrors(error.message, 500, error.code);
+    }
+    throw error;
+  }
 };
