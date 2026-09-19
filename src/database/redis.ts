aac1fdf671a288b9b9749ddba0e83cc950444c3b
AshishResolute <anshugourh21@gmail.com>
2026-09-19 12:22:20 +0530

    Redis upstash connection

--- src/database/redis.ts
+++ src/database/redis.ts
@@ -1,20 +1,14 @@
-import {Redis} from 'ioredis';
-import {REDIS_URL} from '../config.js'
+import { Redis } from "ioredis";
+import { REDIS_URL } from "../config.js";
 
-const redisConnection = new Redis(
- REDIS_URL || 'redis://localhost:6379', 
-  { 
-    maxRetriesPerRequest: null 
-  }
+const redisConnection = new Redis(REDIS_URL || "redis://localhost:6379", {
+  maxRetriesPerRequest: null,
+});
+
+redisConnection.on("error", () => console.log(`Redis connection failed!`));
+
+redisConnection.on("connect", () =>
+  console.log(`Redis connected to upstash server!`),
 );
-
-console.log('Connecting to:', REDIS_URL || 'redis://localhost:6379');
-console.log('Redis host:', redisConnection.options.host);
-console.log('Redis port:', redisConnection.options.port);
-redisConnection.on('error',()=>console.log(`Redis connection failed!`));
-
-
-redisConnection.on('connect',()=>console.log(`Redis connected to upstash server!`));
-
 
 export default redisConnection;
